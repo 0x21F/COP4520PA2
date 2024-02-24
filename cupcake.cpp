17f44b109cfcfbb805519d255d893594b3d9fff5
Carl Campbell <carl-campbell-III@yandex.com>
2024-02-23 23:25:49 -0500

    Cupcake Fixed

--- cupcake.cpp
+++ cupcake.cpp
@@ -17,51 +17,54 @@ void run_first() {
 		if(flags[first].load() == 1) {
 			// if there is a cupcake eat it
 			if(cupcake.load() == true) {
-				std::cout << "partygoer " << first <<  " ate the cupcake. Currently at " << count << ".\n";
-				count++;
+				std::cout << "partygoer " << first <<  " ate the cupcake. Currently at " << ++count << ".\n";
 				cupcake.store(false);
 			}
-		}
 
-		// check if count is n to terminate
-		if(count == n) {
-			// let the minotaur know 
-			flags[first].store(2);
-			break;
-		}
+			// check if count is n to terminate
+			if(count == n) {
+				// let the minotaur know 
+				flags[first].store(2);
+				break;
+			}
 
-		// otherwise return back home
-		flags[first].store(0);
+			// otherwise return back home
+			flags[first].store(0);
+		}
 	}
 }
 
 void run_plebs(int i) {
 	bool visited = false;
-	while(true)  {
+	while(flags[first] != 2)  {
 		// check if it's your turn in the maze
 		// if you visited already just ignore it and finish the maze
 		if(flags[i].load() == 1 && !visited) {
 			// if there is no cupcake, put one there
 			if(cupcake.load() == false) {
 				std::cout << "partygoer " << i <<  " returned the cupcake\n";
+				visited = true;
 				cupcake.store(true);
 			}
 		}
 
 		// otherwise return back home
-		flags[first].store(0);
+		flags[i].store(0);
+		usleep(50);
 	}
 }
 
 
 int main() {
-	std::srand(std::time(nullptr));
-	first = std::rand() % n;
+	// thread here is arbitrary. Shouldn't have any effect on the code
+	//
+	first = 3;
 	std::cout << "running 8 threads\n"; 
 	std::cout << "first thread is " << first << "\n";
 
 	// give threads jobs
 	for(int i=0; i < n; i++) {
+		flags[i] = 0;
 		if(i != first) {
 			partygoers[i] = std::thread(run_plebs, i);
 		}
@@ -69,6 +72,9 @@ int main() {
 			partygoers[i] = std::thread(run_first);
 		}
 	}
+	// sending first person
+	std::cout << "Sending partygoer " << first << " through the maze\n";
+	flags[first].store(1);
 	
 	// here we act as the minotaur
 	while(flags[first].load() != 2) {
@@ -77,9 +83,13 @@ int main() {
 		flags[visitor].store(1);
 		// wait til they finish the maze
 		while(flags[visitor].load() == 1) {
-			usleep(5000);
+			usleep(50);
 		}
 	}
 
 	std::cout << "Minotaur got news that all guests have gone through the maze\n";
+
+	for(int i=0; i < n; i++) {
+		partygoers[i].join();
+	}
 }
